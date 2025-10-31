44fcbf26ee7b962414537bafdf8657f1313b0037
Tae <teazazaqq@gmail.com>
2025-10-31 14:03:44 +0700

    [RED] Add failing test for testHelloNull (null case)

--- src/test/java/com/example/GreetingTest.java
+++ src/test/java/com/example/GreetingTest.java
@@ -3,20 +3,15 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class GreetingTest {
-
-    @Test
-    public void testHelloBasic() {
-        Greeting greeting = new Greeting();
-        assertEquals("Hello, World", greeting.sayHello("World"));
-    }
-
-    @Test
-    ppackage com.example;
-
-    public class Greeting {
-        public String sayHello(String name) {
-            return "Hello, " + name;
+    public class GreetingTest {
+        @Test
+        public void testHelloBasic() {
+            Greeting greeting = new Greeting();
+            assertEquals("Hello, World", greeting.sayHello("World"));
+        }
+        @Test
+       public void testHelloNull() {
+           Greeting greeting = new Greeting();
+           assertEquals("Hello, Friend", greeting.sayHello(null));
         }
     }
-
